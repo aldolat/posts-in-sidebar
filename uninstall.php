942703d8a4f7cda87bdf0905d223ec81f972fc43
aldolat <aldolat@gmail.com>
2013-08-15 19:25:18 +0200

    WP coding standards

--- uninstall.php
+++ uninstall.php
@@ -7,8 +7,8 @@
  */
 
 // Check for the 'WP_UNINSTALL_PLUGIN' constant, before executing
-if( !defined( 'ABSPATH' ) && !defined( 'WP_UNINSTALL_PLUGIN' ) )
+if( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) )
 	exit();
 
-// Delete options from the database
+// Delete widget's options from the database
 delete_option( 'widget_pis_posts_in_sidebar' );

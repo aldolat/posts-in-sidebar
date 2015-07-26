3760a740718f7de18f7ed6ca6781ff996ce8e999
aldolat <aldolat@gmail.com>
2015-07-26 21:08:02 +0200

    Changed javascript, almost final

--- js/pis.js
+++ js/pis.js
@@ -5,11 +5,8 @@ function pis_slidetoggle() {
 	jQuery('.pis-widget-title, .pis-title-center').click(function() {
 		jQuery(this).next().slideToggle('fast');
 	});
-	/*jQuery('.pis-title-center').click(function() {
-		jQuery(this).next().slideToggle('fast');
-	});*/
 }
 
-jQuery(document).on('widget-added', function() { pis_slidetoggle(); });
+jQuery(document).on('widget-added widget-updated', function() { pis_slidetoggle(); });
 jQuery(document).ready(function() { pis_slidetoggle(); });
-jQuery(document).on('widget-updated', function() { pis_slidetoggle(); });
+//jQuery(document).on('widget-updated', function() { pis_slidetoggle(); });

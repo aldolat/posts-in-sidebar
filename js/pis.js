61ec51aa052f776e4423a44aa5d2f9800f97a37d
aldolat <aldolat@gmail.com>
2015-07-26 14:57:00 +0200

    Changed javascript, almost final

--- js/pis.js
+++ js/pis.js
@@ -1,26 +1,6 @@
 /*
  * Posts in Sidebar javascript for admin UI
  */
-/*
-jQuery(document).ready(function(){
-	jQuery('.pis-widget-title').click(function() {
-		jQuery(this).next().slideToggle('fast');
-	});
-	jQuery('.pis-title-center').click(function() {
-		jQuery(this).next().slideToggle('fast');
-	});
-});
-
-jQuery(document).on('widget-updated', function(){
-	jQuery('.pis-widget-title').click(function() {
-		jQuery(this).next().slideToggle('fast');
-	});
-	jQuery('.pis-title-center').click(function() {
-		jQuery(this).next().slideToggle('fast');
-	});
-});
-*/
-
 function pis_slidetoggle() {
 	jQuery('.pis-widget-title').click(function() {
 		jQuery(this).next().slideToggle('fast');

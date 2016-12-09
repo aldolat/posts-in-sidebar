e1fe2833e207feb2cb8e3792ab6191a5bad7a73a
aldolat <aldolat@gmail.com>
2016-12-09 12:01:22 +0100

    Edited a comment in widget admin

--- inc/pis-widget.php
+++ inc/pis-widget.php
@@ -2674,7 +2674,7 @@ class PIS_Posts_In_Sidebar extends WP_Widget {
 										$this->get_field_name( 'tax_name' ),
 										esc_attr( $instance['tax_name'] ),
 										__( 'genre', 'posts-in-sidebar' ),
-										sprintf( __( 'Enter the term name of the custom taxonomy (e.g., %1$sgenre%2$s).%3$sUse this field only if you selected "Custom taxonomy" in the "Link to the archive of" dropdown menu.', 'posts-in-sidebar' ), '<code>', '</code>', '<br />' ),
+										sprintf( __( 'Enter the term name of the custom taxonomy (e.g., %1$sgenre%2$s).%3$sUse this field only if you selected "Custom taxonomy" in the "Link to the archive of" dropdown menu.%4$s', 'posts-in-sidebar' ), '<code>', '</code>', '<br /><strong>', '</strong>' ),
 										'margin: 0; padding: 0.5em;'
 									); ?>
 								</div>

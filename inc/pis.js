45d956aba4ab274ce5c938a35907538180a0606a
aldolat <aldolat@gmail.com>
2017-05-07 20:23:02 +0200

    WP codig standards for js

--- inc/pis.js
+++ inc/pis.js
@@ -6,48 +6,50 @@
   * @since 2.0
   * @since 3.8.9 Now the panels remain open if Save button is clicked.
   */
- jQuery(document).ready( function(event) {
+ jQuery( document ).ready( function( event ) {
     // The namespace
     var pis_namespace = {
 		// Holds an array of open panels per wiget id
     	open_panels : {},
         // Generic click handler on the panel title
-        clickHandler: function(element) {
+        clickHandler: function( element ) {
 			// Open the div "below" the h4 title
-			jQuery(element).toggleClass('open').next().stop().slideToggle();
+			jQuery( element ).toggleClass( 'open' ).next().stop().slideToggle();
 			// Get the data-panel attribute, for example "custom-taxonomy-query"
-			var panel = element.getAttribute('data-panel');
+			var panel = element.getAttribute( 'data-panel' );
 			// Get the id of the widget, for example "widget-32_pis_posts_in_sidebar-8", in parent panels
-			var id = jQuery(element).parent().parent().parent().parent().parent().attr('id');
+			var id = jQuery( element ).parent().parent().parent().parent().parent().attr( 'id' );
 			// Get the id of the widget, for example "widget-32_pis_posts_in_sidebar-8", in child panels
 			if ( id === undefined )
-				var id = jQuery(element).parent().parent().parent().parent().parent().parent().parent().attr('id');
+				var id = jQuery( element ).parent().parent().parent().parent().parent().parent().parent().attr( 'id' );
 			var o = {};
-			if (this.open_panels.hasOwnProperty(id))
+			if ( this.open_panels.hasOwnProperty( id ) ) {
 				o = this.open_panels[id];
-			if (o.hasOwnProperty(panel))
+			}
+			if ( o.hasOwnProperty( panel ) ) {
 				delete o[panel];
-			else
+			} else {
 				o[panel] = true;
+			}
 			this.open_panels[id] = o;
         }
     }
 
- 	jQuery('.pis-widget-title').click(function () {
-        pis_namespace.clickHandler(this);
+ 	jQuery( '.pis-widget-title' ).click( function() {
+        pis_namespace.clickHandler( this );
  	});
 
  	// After saving the widget, we need to reassign click handlers
- 	jQuery(document).on('widget-updated', function(root,element){
- 		jQuery('.pis-widget-title').off('click').on('click', function () {
-            pis_namespace.clickHandler(this);
- 		})
-         // Refresh panels to the state before saving
-        var id = jQuery(element).attr('id');
-        if (pis_namespace.open_panels.hasOwnProperty(id)) {
+ 	jQuery( document ).on( 'widget-updated', function( root, element ) {
+ 		jQuery( '.pis-widget-title' ).off( 'click' ).on( 'click', function() {
+            pis_namespace.clickHandler( this );
+ 		});
+        // Refresh panels to the state before saving
+        var id = jQuery( element ).attr( 'id' );
+        if ( pis_namespace.open_panels.hasOwnProperty( id ) ) {
             var o = pis_namespace.open_panels[id];
-            for (var panel in o) {
-                jQuery(element).find('[data-panel='+panel+']').toggleClass('open').next().stop().show();
+            for ( var panel in o ) {
+                jQuery( element ).find( '[data-panel=' + panel + ']' ).toggleClass( 'open' ).next().stop().show();
             }
         }
  	});
